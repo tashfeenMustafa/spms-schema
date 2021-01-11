493f4d42e80104eb9a68f1214eb60e42c0bac09b
Shohan20lac <63243156+Shohan20lac@users.noreply.github.com>
2021-01-12 02:23:47 +0600

    Add files via upload
    SQL queries for adding 10 prerequisite pairs to start with.

---
+++ 5. populate (prereqs).sql
@@ -0,0 +1,46 @@
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE203', 'CSE201');
+
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE201', 'CSC101');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE104', 'PHY102');
+
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE204', 'CSE104');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE210', 'CSE104');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE211', 'CSE203');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE213', 'CSE203');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE214', 'CSE204');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE216', 'CSE214');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE303', 'CSE203');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE307', 'CSE213');
+    
+INSERT INTO prerequisite_course	(courseID, prerequisiteCourseID)
+	VALUES	('CSE307', 'CSE303');
+    
+
+    
+    
+    
+    
+
+    
+
+
+    
