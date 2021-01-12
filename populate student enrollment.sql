d6d2183df3a53743d5a311b34bd64c489d8a8f1b
Madihamohammadishaque <77229999+Madihamohammadishaque@users.noreply.github.com>
2021-01-12 17:33:50 +0600

    Add files via upload
    Add student enrollment

---
+++ populate student enrollment.sql
@@ -0,0 +1,8 @@
+INSERT INTO STUDENT_ENROLLMENT (enrolledCourseID, sAccountID, semesterSeason,  semesterYear, sectionNumber, gradeReceived)
+VALUES ('CSE303' , '1742892' ,'SUMMER', '2020', '02','B+');
+
+INSERT INTO STUDENT_ENROLLMENT (enrolledCourseID, sAccountID, semesterSeason,  semesterYear, sectionNumber, gradeReceived)
+VALUES ('CSE303' , '1819230' ,'SUMMER', '2020', '02','A');
+
+INSERT INTO STUDENT_ENROLLMENT (enrolledCourseID, sAccountID, semesterSeason,  semesterYear, sectionNumber, gradeReceived)
+VALUES ('CSE303' , '1819230' ,'SUMMER', '2020', '02','A');
